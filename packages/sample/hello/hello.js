8733d1b3fb993d8ebc003963533c4087e4b0f763
Osprey1999 <hiabheek@gmail.com>
2023-09-23 22:33:07 +0530

    chore: updated hello.js
    Co-authored-by: Osprey1999 <hiabheek@gmail.com>

--- packages/sample/hello/hello.js
+++ packages/sample/hello/hello.js
@@ -1,34 +1,17 @@
 //returning a static JSON file as a response Body 
 
 function main(args) {
-    const fs = require("fs");
+  const responseData = {
+    statusCode : 200,
+    headers : {
+      'content-type' : 'application/json'
+    },
+    body : {
+      "name": "abheek"
+    }
+  }  
 
-    //reading the json file using fs 
-    fs.readFile("sample.json", (error,data)=>{
-      if(error){
-        console.error(error);
-        return;
-      }
-
-      //parse the json data
-      let jsonData  = JSON.parse(data);
-
-      //response 
-      const responseData = {
-          statusCode : 200,
-          headers : {
-            'content-type' : 'application/json'
-          },
-          body : JSON.stringify(jsonData)  //converting the jsonData into JSON
-      }
-
-      //printing the JSON response into the console
-      console.log(responseData);
-
-      return(responseData);
-    })
-    
-
-  }
+  return responseData
+}
 
 exports.main = main;
